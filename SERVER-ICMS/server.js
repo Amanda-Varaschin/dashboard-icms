2599959e7a012853cc6df8026c6a552ff9b639c0
[puroOdio046] <[kadufelipy2013@hotmail.com]>
2025-03-04 17:14:27 -0300

    Alteração next.config

--- SERVER-ICMS/server.js
+++ SERVER-ICMS/server.js
@@ -145,6 +145,6 @@ app.get('/download-csv-siconfi', (req, res) => {
 processData();
 
 // Inicia o servidor Express
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
--- dashboard-icms/next.config.mjs
+++ dashboard-icms/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      API_BASE_URL: "https://server-icms.onrender.com",
+      TESOURO_URL: "https://server-icms.onrender.com/dados-json-tesouro",
+      SICONFI_URL: "https://server-icms.onrender.com/dados-json-siconfi"
+    }
+  };
+  
+  export default nextConfig;
+  
