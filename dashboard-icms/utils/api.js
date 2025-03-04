fff5ad0dd37fae686be39ae02133f34aebc12e15
[puroOdio046] <[kadufelipy2013@hotmail.com]>
2025-03-04 17:03:39 -0300

    Correção das URLs e ajustes no código

---
+++ dashboard-icms/utils/api.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = "https://server-icms.onrender.com";
+export const TESOURO_URL = `${API_BASE_URL}/dados-json-tesouro`;
+export const SICONFI_URL = `${API_BASE_URL}/dados-json-siconfi`;
