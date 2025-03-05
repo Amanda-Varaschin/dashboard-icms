5a18c66aed8bdda7c4af5abb65c41a12bbc12fa2
[puroOdio046] <[kadufelipy2013@hotmail.com]>
2025-03-05 11:51:54 -0300

    Alteração da url API_BASE_URL next.config

--- dashboard-icms/next.config.js
+++ dashboard-icms/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_BASE_URL: "https://server-icms.onrender.com",
+    NEXT_PUBLIC_API_BASE_URL: "https://dashboard-icms.onrender.com/",
   },
 };
 
